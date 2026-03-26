5fe3f01c6f22c225cecce43c40015718392e46f3
Jadson <jadsonzap@gmail.com>
2026-03-26 00:15:53 -0300

    add link backend

--- src/services/apiLogin.js
+++ src/services/apiLogin.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const api = axios.create({ baseURL: `http://localhost:5000/api` });
+export const api = axios.create({ baseURL: `https://helogin-api.onrender.com/api` });
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
