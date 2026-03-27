faa78e8d94d9df4299a7cf80906bdf80727c4223
Jadson <jadsonzap@gmail.com>
2026-03-27 20:04:14 -0300

    Remove @vercel/speed-insights;

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 export default function App() {
   return (
-   <SpeedInsights>
     <RouterProvider router={router} />
-   </SpeedInsights>
   );
 }
